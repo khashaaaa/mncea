aa9ac19538d58d3e28cf7653008d140d68ff6dcc
khashaaaa <forwardent01@gmail.com>
2023-12-18 00:24:12 +0800

    Menu become dynamic

--- workspaces/interface/src/components/ShowCase.jsx
+++ workspaces/interface/src/components/ShowCase.jsx
@@ -1,106 +1,88 @@
 import { IconChevronDown, IconChevronUp } from '@tabler/icons-react'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 export const ShowCase = () => {
-
     const [mega, setMega] = useState(0)
+    const [baseCategories, setBaseCategories] = useState([])
+    const [midCategories, setMidCategories] = useState([])
+    const [subCategories, setSubCategories] = useState([])
+
+    useEffect(() => {
+        GetMenuData()
+    }, [])
+
+    const GetMenuData = async () => {
+        try {
+            const [baseRaw, midRaw, subRaw] = await Promise.all([
+                fetch(`/server/basecategory`),
+                fetch(`/server/midcategory`),
+                fetch(`/server/subcategory`),
+            ])
+
+            const [baseResp, midResp, subResp] = await Promise.all([
+                baseRaw.json(),
+                midRaw.json(),
+                subRaw.json(),
+            ])
+
+            setBaseCategories(baseResp)
+            setMidCategories(midResp)
+            setSubCategories(subResp)
+        } catch (error) {
+            console.error("Error fetching menu data:", error)
+        }
+    }
+
+    const renderMidCategories = (parentMark) => {
+        return midCategories
+            .filter((cat) => cat.parent === parentMark)
+            .map((cat, num) => (
+                <div key={cat.mark}>
+                    <p className="font-bold">{cat?.name}</p>
+                    {renderSubCategories(cat.mark)}
+                </div>
+            ))
+    }
+
+    const renderSubCategories = (midCategoryMark) => {
+        return subCategories
+            .filter((cat) => cat.parent === midCategoryMark)
+            .map((cat, num) => (
+                <p key={cat.mark} className="ml-4">{cat?.name}</p>
+            ))
+    }
 
     return (
         <div className="bg-stone-100 py-4">
             <div className="relative" style={{ margin: '0 15%' }}>
                 <div className="grid grid-cols-3 gap-8">
-                    <div onMouseEnter={() => setMega(1)} onMouseLeave={() => setMega(0)} className={`${mega === 1 ? "rounded-t bg-blue-800" : "rounded-md bg-main"} text-white flex items-center justify-center py-2 cursor-pointer`}>
-                        <p className="font-bold uppercase">Чанарын баталгаажуулалт</p>
-                        {
-                            mega === 1 ?
-                                <IconChevronUp size={18} />
-                                :
-                                <IconChevronDown size={18} />
-                        }
-                    </div>
-                    <div onMouseEnter={() => setMega(2)} onMouseLeave={() => setMega(0)} className={`${mega === 2 ? "rounded-t bg-blue-800" : "rounded-md bg-main"} text-white flex items-center justify-center py-2 cursor-pointer`}>
-                        <p className="font-bold uppercase">Мэргэшлийн үндэсний шаталсан бүтэц</p>
-                        {
-                            mega === 2 ?
-                                <IconChevronUp size={18} />
-                                :
-                                <IconChevronDown size={18} />
-                        }
-                    </div>
-                    <div onMouseEnter={() => setMega(3)} onMouseLeave={() => setMega(0)} className={`${mega === 3 ? "rounded-t bg-blue-800" : "rounded-md bg-main"} text-white flex items-center justify-center py-2 cursor-pointer`}>
-                        <p className="font-bold uppercase">Боловсролын зэрэг</p>
-                        {
-                            mega === 3 ?
-                                <IconChevronUp size={18} />
-                                :
-                                <IconChevronDown size={18} />
-                        }
-                    </div>
+                    {baseCategories.map((cat, num) => {
+                        let active = num + 1
+                        return (
+                            <div
+                                key={cat.mark}
+                                onMouseEnter={() => setMega(active)}
+                                onMouseLeave={() => setMega(0)}
+                                className={`${mega === active ? "rounded-t bg-blue-800" : "rounded-md bg-main"} text-white flex items-center justify-center py-2 cursor-pointer`}
+                            >
+                                <p className="font-bold uppercase">{cat?.name}</p>
+                                {mega === active ? <IconChevronUp size={18} /> : <IconChevronDown size={18} />}
+                            </div>
+                        )
+                    })}
                 </div>
 
                 <div onMouseLeave={() => setMega(0)} className={`${mega === 0 && 'hidden'} absolute grid grid-cols-3 gap-8 w-full bg-main rounded-md z-10 shadow-xl`}>
                     <div onMouseEnter={() => setMega(1)} className={mega === 1 ? "bg-blue-800 text-white rounded-bl p-2" : "p-2 text-gray-200"}>
-                        <div>
-                            <p className="font-bold">Чанарын баталгаажуулалт</p>
-                            <p className="ml-4">Зөвлөмж</p>
-                            <p className="ml-4">ДЧБ-ын тогтолцоо</p>
-                            <p className="ml-4">БСБ-ын ДЧБ-ын мэдээлэл</p>
-                        </div>
-                        <div>
-                            <p className="font-bold">Магадлан итгэмжлэл</p>
-                            <p className="ml-4">Дүрэм журам</p>
-                            <p className="ml-4">Загвар, маягтууд</p>
-                            <p className="ml-4">Зөвлөмж</p>
-                            <p className="ml-4">МИ системд нэвтрэх</p>
-                        </div>
-                        <div>
-                            <p className="font-bold">Тусгай зөвшөөрөл</p>
-                            <p className="ml-4">Дотоодын байгууллага эрх хүсэх</p>
-                            <p className="ml-4">Гадаадын байгууллага эрх хүсэх</p>
-                        </div>
-                        <div>
-                            <p className="font-bold">Статистик</p>
-                            <p className="ml-4">МИ сургалтын байгууллагын тоон мэдээлэл</p>
-                            <p className="ml-4">МИ сургалтын хөтөлбөрийн тоон мэдээлэл</p>
-                            <p className="ml-4">МИ сургалтын хөтөлбөрийн тоон мэдээлэл</p>
-                        </div>
+                        {renderMidCategories(5)}
                     </div>
 
                     <div onMouseEnter={() => setMega(2)} className={mega === 2 ? "bg-blue-800 text-white p-2" : "p-2 text-gray-200"}>
-                        <div>
-                            <p className="font-bold">МҮШБ гэж юу вэ?</p>
-                            <p className="ml-4">Зураглал</p>
-                            <p className="ml-4">Дүрэм, журам</p>
-                            <p className="ml-4">МҮШБ мэдээлэл</p>
-                            <p className="ml-4">Загвар маягтууд</p>
-                            <p className="ml-4">Зөвлөмж</p>
-                        </div>
-                        <div>
-                            <p className="font-bold">Олон улсын МҮШБ</p>
-                            <p className="ml-4">EQF</p>
-                            <p className="ml-4">Улс орнуудын МҮШБ судалгаа</p>
-                        </div>
+                        {renderMidCategories(6)}
                     </div>
 
                     <div onMouseEnter={() => setMega(3)} className={mega === 3 ? "bg-blue-800 text-white rounded-br p-2" : "p-2 text-gray-200"}>
-                        <div>
-                            <p className="font-bold">Боловсролын тогтолцоо</p>
-                            <p className="ml-4">Монголын боловсролын тогтолцоо</p>
-                            <p className="ml-4">Дээд боловсрол эзэмших</p>
-                            <p className="ml-4">Зөвлөмж</p>
-                            <p className="ml-4">Дүрэм, журам</p>
-                            <p className="ml-4">Загвар маягтууд</p>
-                            <p className="ml-4">Дүйцүүлэлт</p>
-                            <p className="ml-4">Баталгаажуулалт</p>
-                        </div>
-                        <div>
-                            <p className="font-bold">Олон улсын боловсролын тогтолцоо</p>
-                            <p className="ml-4">EU</p>
-                            <p className="ml-4">ASIA</p>
-                            <p className="ml-4">AFRICA</p>
-                            <p className="ml-4">AU</p>
-                            <p className="ml-4">AMERICA</p>
-                        </div>
+                        {renderMidCategories(7)}
                     </div>
                 </div>
             </div>
